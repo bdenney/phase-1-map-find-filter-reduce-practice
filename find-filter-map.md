16f541836fe417da4bbd0cd240ef1d06fe58f898
Brandon Denney <ee.brandon@gmail.com>
2022-02-03 17:45:07 -0500

    Added practice files.

---
+++ find-filter-map.md
@@ -0,0 +1,11 @@
+# Extra Practice for Find, Filter, Map
+
+- Create a function called `findMichael` that returns an array of the movies in `HALLOWEEN_MOVIES` that contain the string `Michael Myers`. You **must** use `array.filter()`!
+
+- Create a function called `newHalloweenMovies` that takes one argument called that is a year. Return an array of all movies that were released **after** the given year. You **must** use `array.filter()`!
+
+- Create a function called `dontCountMichael` that returns the count of movies that **do not** have the string `Michael Myers` in the name. You **must** use `array.reduce()`!
+
+- Create a function called `averageYear` that returns the average of the year of all movies in `HALLOWEEN_MOVIES`.
+
+- Create a function called `theLastWord` that returns the last word of each movie in `HALLOWEEN_MOVIES`. You **must** use `array.map()`!
---
+++ index.js
@@ -0,0 +1,46 @@
+// Our data.
+const HALLOWEEN_MOVIES = [
+    { name: "Halloween", release_year: 1978 },
+    { name: "Halloween II", release_year: 1981 },
+    { name: "Halloween III: Season of the Witch", release_year: 1982 },
+    { name: "Halloween 4: The Return of Michael Myers", release_year: 1988 },
+    { name: "Halloween 5: The Revenge of Michael Myers", release_year: 1989 },
+    { name: "Halloween: The Curse of Michael Myers", release_year: 1995 },
+    { name: "Halloween H20: 20 Years Later", release_year: 1998 },
+    { name: "Halloween Resurrection", release_year: 2002 },
+    { name: "Halloween", release_year: 2007 },
+    { name: "Halloween II", release_year: 2009 },
+    { name: "Halloween", release_year: 2018 },
+    { name: "Halloween Kills", release_year: 2021 },
+    { name: "Halloween Ends", release_year: 2022 }
+]
+
+function findMichael() {
+    return HALLOWEEN_MOVIES.filter(movie => movie.name.includes("Michael Myers"));
+}
+
+function newHalloweenMovies(year) {
+    return HALLOWEEN_MOVIES.filter(movie => movie.release_year > year);
+}
+
+function dontCountMichael() {
+    return HALLOWEEN_MOVIES.reduce((previous, current) => {
+        if (!current.name.includes("Michael Myers")) {
+            return previous + 1;
+        } else {
+            return previous;
+        }
+    });
+}
+
+function theLastWord() {
+    return HALLOWEEN_MOVIES.map(item => {
+        itemArr = item.name.split(' ');
+        return itemArr[itemArr.length - 1];
+    });
+}
+
+console.log(findMichael());
+console.log(newHalloweenMovies(2000));
+console.log(dontCountMichael());
+console.log(theLastWord());
